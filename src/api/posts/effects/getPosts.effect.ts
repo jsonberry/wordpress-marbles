92982d5947a3fc93b521448672b0a81ec95a86ef
jsonberry <jason.s.awbrey@gmail.com>
2018-11-25 12:21:57 -0800

    chore: add null checks

--- src/api/posts/effects/getPosts.effect.ts
+++ src/api/posts/effects/getPosts.effect.ts
@@ -1,6 +1,7 @@
 import { Effect, HttpError, HttpStatus } from '@marblejs/core';
 import { AxiosResponse } from 'axios';
 import { throwError } from 'rxjs';
+import { hasProps } from 'rxjs-toolkit';
 import { catchError, mergeMap, pluck, reduce, switchMap } from 'rxjs/operators';
 import { bodyResTransducer } from '../../common';
 import { postTransducer } from '../helpers';
@@ -10,6 +11,7 @@ export const getPostsEffect$: Effect = req$ =>
   req$.pipe(
     pluck('query'),
     switchMap(postsDao.posts$),
+    hasProps('data'),
     pluck<AxiosResponse, any[]>('data'),
     mergeMap(data => data),
     postTransducer,
