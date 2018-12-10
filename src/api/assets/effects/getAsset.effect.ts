e83d141dcbbe6f5c78db0ab35ca72cbd988d587e
Jason Awbrey <jason.s.awbrey@gmail.com>
2018-12-10 13:35:06 -0800

    feat: add apidoc (#19)

--- src/api/assets/effects/getAsset.effect.ts
+++ src/api/assets/effects/getAsset.effect.ts
@@ -4,6 +4,21 @@ import { catchError } from 'rxjs/operators';
 import { bodyResTransducer } from '../../../common';
 import appServices from '../../../services';
 
+/**
+ * @api {get} /assets/:id Request Specific Asset
+ * @apiName getAssetEffect$
+ * @apiGroup Assets
+ *
+ * @apiSuccess (200) {Object} asset A media asset, most likely an image
+ * @apiSuccess {Number} asset._wp_id WordPress Asset ID
+ * @apiSuccess {String} asset.id ID of Asset - the slug from WordPress
+ * @apiSuccess {Number} asset.post The WP Post ID
+ * @apiSuccess {String} asset.thumbnail Asset thumbnail sized URI
+ * @apiSuccess {String} asset.medium Asset medium sized URI
+ * @apiSuccess {String} asset.medium_large Asset medium_large sized URI
+ * @apiSuccess {String} asset.large Asset large sized URI
+ * @apiSuccess {String} asset.full Asset full sized URI
+ */
 export const getAssetEffect$: Effect = req$ =>
   req$.pipe(
     appServices.assets.entity$,
